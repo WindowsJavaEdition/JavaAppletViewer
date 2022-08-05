32a1eb0f868ee4ae15c2f9b6b4685bdd2a46de9a
piXel <48454166+piXelisgaming@users.noreply.github.com>
2022-08-05 22:53:57 +0200

    Added the source code
    abcyusbaucbasucbauydbnaufbc

---
+++ JavaAppletViewer/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim root As String = System.Windows.Forms.Application.StartupPath
+        WebBrowser1.Navigate(root + "\launcher.html")
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(ByVal sender As System.Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+
+    End Sub
+
+    Private Sub RestartToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RestartToolStripMenuItem.Click
+        Application.Restart()
+    End Sub
+
+    Private Sub FileToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FileToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub ConfigureToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ConfigureToolStripMenuItem.Click
+        Form2.Show()
+    End Sub
+
+    Private Sub GoToGithubPageToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GoToGithubPageToolStripMenuItem.Click
+        Process.Start("https://github.com/WindowsJavaEdition/JavaAppletViewer")
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+        Form3.Show()
+    End Sub
+End Class

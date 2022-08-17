81caebc43a21c32e4f11a40652c61464396ed2d7
piXel <48454166+piXelisgaming@users.noreply.github.com>
2022-08-17 17:55:36 +0200

    i did an oopsie

--- JavaAppletViewer/Module1.vb
+++
@@ -1,5 +0,0 @@
-﻿Module Module1
-    ' for global variables (root directory and easter egg)
-    Public EasterEgg As Integer = 0
-    Public root As String = System.Windows.Forms.Application.StartupPath
-End Module
